db5392ee171f07808965c33792d860879f686998
NineZ123 <71476905+NineZ123@users.noreply.github.com>
2020-11-07 18:46:51 -0800

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,26 @@
+class bob{
+    constructor(x, y,radius) {
+        var options = {
+            isStatic : false,
+            restitution : 1,
+            density : 1
+        }
+    this.body = Bodies.circle(x ,y,radius,options);
+
+    this.radius=radius;
+
+    World.add(world,this.body);
+    
+    }
+    
+    display(){
+        var pos =this.body.position;
+        ellipseMode(CENTER);
+        fill("pink");
+        ellipse(pos.x,pos.y,this.radius);
+      pop();
+    }
+
+    
+    
+}
